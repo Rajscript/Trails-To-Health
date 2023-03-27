7a524d56f692c0d8a7dbe03addf7ecfbba29ea00
R D <rd@Rs-Laptop.local>
2023-03-27 16:48:29 -0400

    added

--- App/App.py
+++ App/App.py
@@ -116,4 +116,4 @@ def recommend():
     return render_template('recommend.html', recommendations=recommendations_table)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
